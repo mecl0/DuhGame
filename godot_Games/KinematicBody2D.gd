f7d2c6b9aeaa67763585753488767bebe1fbd8c6
lamo <lamo@pop-os.localdomain>
2020-02-06 11:33:09 -0600

    movement changes 1

--- godot_Games/KinematicBody2D.gd
+++ godot_Games/KinematicBody2D.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 # this is the movement controls
+const Speed = 200
+#const wall Vector2(1,1)
 
 var destination = Vector2()
 var gap = Vector2()
-const Speed = 200
+
 
 func _ready():
 	
@@ -18,18 +20,24 @@ func _process(delta):
 		move_and_slide(gap.normalized() * Speed)
 		
 		#this will snap to the destiation if sprite is close to mouse click
-		if gap.abs() < Vector2(3,3):
+		if gap.abs() < Vector2(2,2):
 			set_position(destination)
 	
 	#this moves stops the movement if destination is == postion
 	if destination == position:
 		move_and_slide(gap*0)
+		
+	if is_on_wall():
+		destination = position
+	
 	
 	
 	pass
 func _input(event):
 	if Input.is_action_just_pressed("ui_mouse"):
 		destination = get_global_mouse_position()
+		
+		#is_colliding()
 		#test add
 	
 
